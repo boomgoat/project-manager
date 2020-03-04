fb2c6d6b6ee4aea0cefb3c50e0d2cf6d597734a4
boomgoat <talhaiqbal11@gmail.com>
2020-03-04 10:14:15 -0800

    feat: router set up

--- src/App.js
+++ src/App.js
@@ -1,14 +1,48 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
 
-function App() {
+// Router Imports
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+// Component Imports
+import { Navbar } from "./components/layout/Navbar";
+import Dashboard from "./components/dashboard/Dashboard";
+import { ProjectDetails } from "./components/projects/ProjectDetails";
+import SignIn from "./components/auth/SignIn";
+import SignUp from "./components/auth/SignUp";
+import CreateProject from "./components/projects/CreateProject";
+
+// Redux Imports
+// import { useSelector, useDispatch } from "react-redux";
+import { connect } from "react-redux";
+
+const App = props => {
+  // const dispatch = useDispatch();
+  const { projects } = props;
   return (
     <BrowserRouter>
-      <div className="App">
-        <h1>My Planner</h1>
+      <div className="App grey darken-4 white-text">
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Dashboard} />
+          <Route
+            path="/project/:id"
+            render={props => (
+              <ProjectDetails projects={projects} cardType="details" />
+            )}
+          />
+          <Route path="/signin" component={SignIn} />
+          <Route path="/signup" component={SignUp} />
+          <Route path="/createproject" component={CreateProject} />
+        </Switch>
       </div>
     </BrowserRouter>
   );
-}
+};
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    projects: state.project.projects
+  };
+};
+
+export default connect(mapStateToProps)(App);
