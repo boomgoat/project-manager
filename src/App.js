33a2519dd281c2f26e92a6d21aa793ec43ddfcfd
boomgoat <talhaiqbal11@gmail.com>
2020-03-11 22:28:00 -0800

    feat: Overlay Display on idle

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 // Router Imports
 import { BrowserRouter, Route, Switch } from "react-router-dom";
@@ -13,18 +13,64 @@ import CreateProject from "./components/projects/CreateProject";
 import { connect } from "react-redux";
 
 // Custom Function Imports
-import { setup } from "./functions/idle";
 
 // Test Imports
 import IdleOverlay from "./components/layout/IdleOverlay";
 
 const App = props => {
+  const [isIdle, setIsIdle] = useState(false);
+
   useEffect(() => {
+    let initialTimer;
+    let finalTimer;
+
+    const setup = () => {
+      document.addEventListener("mousemove", resetTimer, false);
+      document.addEventListener("mousedown", resetTimer, false);
+      document.addEventListener("keypress", resetTimer, false);
+      document.addEventListener("DOMMouseScroll", resetTimer, false);
+      document.addEventListener("mousewheel", resetTimer, false);
+      document.addEventListener("touchmove", resetTimer, false);
+      document.addEventListener("MSPointerMove", resetTimer, false);
+
+      startTimer();
+    };
+
+    const startTimer = () => {
+      // wait 2 seconds before calling goInactive
+      initialTimer = window.setTimeout(goInactive, 5000);
+    };
+
+    const resetTimer = e => {
+      window.clearTimeout(initialTimer);
+      window.clearTimeout(finalTimer);
+
+      goActive();
+    };
+
+    const goInactive = () => {
+      // do something
+      finalTimer = window.setTimeout(goInactiveFinal, 2000);
+      console.log("First Timeout: Second Timer Fired", finalTimer);
+    };
+
+    const goInactiveFinal = () => {
+      console.log("Second Timeout");
+      renderOverlay();
+    };
+
+    const goActive = () => {
+      // do something
+      console.log("Timer Reset");
+      startTimer();
+    };
+
     setup();
   });
 
-  const isIdle = () => {
-    return <IdleOverlay />;
+  const renderOverlay = () => {
+    setIsIdle(true);
+    console.log("isIdle: ", isIdle);
   };
 
   const { auth } = props;
@@ -32,6 +78,7 @@ const App = props => {
   if (!auth.isLoaded) return <span></span>;
   return (
     <BrowserRouter>
+      {isIdle ? <IdleOverlay /> : null}
       <div className="App grey darken-4 white-text">
         <Navbar />
         <Switch>
